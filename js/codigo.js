458dd5bc394391863156427df791a203fafe9bc1
Ginna Mora <higinnamora@gmail.com>
2024-02-08 12:56:22 -0500

    UI: Improvements + meme

--- js/codigo.js
+++ js/codigo.js
@@ -1,98 +1,112 @@
-const c = string => {
-  return console.log(string)
-}
+const c = (string) => {
+  return console.log(string);
+};
 const p = {
-  teclas: document.querySelectorAll('#calculadora ul li'),
-  accion: '',
-  digito: '',
-  operaciones: document.getElementById('operaciones'),
+  teclas: document.querySelectorAll("#calculadora ul li"),
+  accion: "",
+  digito: "",
+  operaciones: document.getElementById("operaciones"),
   cantidadSignos: 0,
   cantidadDecimal: false,
   resultado: false,
-  numeros: Array.from(document.getElementsByClassName('numero')),
-  signos: Array.from(document.getElementsByClassName('signo'))
-}
+  numeros: Array.from(document.getElementsByClassName("numero")),
+  signos: Array.from(document.getElementsByClassName("signo")),
+};
 
 const m = {
   inicio: () => {
-    p.teclas.forEach(e => {
-      e.addEventListener('click', m.oprimirTecla)
-    })
-    document.addEventListener('keydown', m.oprimirTeclado)
+    p.teclas.forEach((e) => {
+      e.addEventListener("click", m.oprimirTecla);
+    });
+    document.addEventListener("keydown", m.oprimirTeclado);
   },
-  numero: digito => {
-    p.cantidadSignos = 0
-    p.operaciones.innerHTML === '0' || p.resultado
+  numero: (digito) => {
+    p.cantidadSignos = 0;
+    p.operaciones.innerHTML === "0" || p.resultado
       ? (p.operaciones.innerHTML = digito)
-      : (p.operaciones.innerHTML += digito)
-    p.resultado = false
+      : (p.operaciones.innerHTML += digito);
+    p.resultado = false;
   },
-  signo: digito => {
+  signo: (digito) => {
     ++p.cantidadSignos === 1
       ? (p.operaciones.innerHTML += digito)
-      : c('mas de un signo')
-    p.cantidadDecimal = false
-    p.resultado = false
+      : c("mas de un signo");
+    p.cantidadDecimal = false;
+    p.resultado = false;
   },
   resultado: () => {
-    p.resultado = true
-    p.cantidadDecimal = true
-    p.operaciones.innerHTML = eval(p.operaciones.innerHTML)
+    p.resultado = true;
+    p.cantidadDecimal = true;
+    p.operaciones.innerHTML = eval(p.operaciones.innerHTML);
   },
-  decimal: digito => {
+  decimal: (digito) => {
     if (!p.cantidadDecimal && p.cantidadSignos === 0) {
-      p.operaciones.innerHTML += digito
-      p.cantidadDecimal = true
+      p.operaciones.innerHTML += digito;
+      p.cantidadDecimal = true;
     }
-    p.resultado = false
+    p.resultado = false;
   },
-  oprimirTecla: tecla => {
-    p.accion = tecla.target.getAttribute('class')
-    p.digito = tecla.target.innerHTML
-    m.calculadora(p.accion, p.digito)
+  oprimirTecla: (tecla) => {
+    p.accion = tecla.target.getAttribute("class");
+    p.digito = tecla.target.innerHTML;
+    m.calculadora(p.accion, p.digito);
   },
-  oprimirTeclado: tecla => {
-    let numero = p.numeros.find(element => {
-      return element.innerHTML === tecla.key
-    })
-    let signo = p.signos.find(element => {
-      return element.innerHTML === tecla.key
-    })
-    tecla.key === 'Enter'
+  oprimirTeclado: (tecla) => {
+    let numero = p.numeros.find((element) => {
+      return element.innerHTML === tecla.key;
+    });
+    let signo = p.signos.find((element) => {
+      return element.innerHTML === tecla.key;
+    });
+    tecla.key === "Enter"
       ? m.resultado()
-      : tecla.key === '.'
-        ? m.decimal(tecla.key)
-        : numero
-          ? m.numero(numero.innerHTML)
-          : signo
-            ? m.signo(signo.innerHTML)
-            : tecla.key === 'Delete' ? m.borrar() : c('caracter no valido')
+      : tecla.key === "."
+      ? m.decimal(tecla.key)
+      : numero
+      ? m.numero(numero.innerHTML)
+      : signo
+      ? m.signo(signo.innerHTML)
+      : tecla.key === "Delete"
+      ? m.borrar()
+      : c("caracter no valido");
   },
   borrar: () => {
-    p.operaciones.innerHTML = 0
-    p.cantidadSignos = 0
-    p.cantidadDecimal = false
-    p.resultado = false
+    p.operaciones.innerHTML = 0;
+    p.cantidadSignos = 0;
+    p.cantidadDecimal = false;
+    p.resultado = false;
   },
   calculadora: (accion, digito) => {
     switch (accion) {
-      case 'numero':
-        m.numero(digito)
-        break
-      case 'signo':
-        m.signo(digito)
-        break
-      case 'igual':
-        m.resultado()
-        break
-      case 'decimal':
-        m.decimal('.')
-        break
+      case "numero":
+        m.numero(digito);
+        break;
+      case "signo":
+        m.signo(digito);
+        break;
+      case "igual":
+        m.resultado();
+        break;
+      case "decimal":
+        m.decimal(".");
+        break;
 
       default:
-        break
+        break;
     }
-  }
-}
+  },
+};
 
-m.inicio()
+m.inicio();
+
+// Meme
+const vecina = document.querySelector(".vecina");
+const hazClick = document.querySelector(".haz-click");
+
+vecina.addEventListener("click", hiddeVecina);
+hazClick.addEventListener("click", hiddeVecina);
+
+function hiddeVecina() {
+  vecina.style.display = "none";
+  hazClick.style.display = "none";
+}
